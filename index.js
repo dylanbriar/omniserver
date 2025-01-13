f0b9c8756c3bb534a322923d33b746411cecdbe7
Dylan Compton <dylancomptontx@gmail.com>
2025-01-12 18:23:49 -0600

    pg up

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 //require for servers, import for client (generally)
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+// example query to reference
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
 
 const whitelist = ['https://www.mybudgetree.com', 'http://localhost:5173']
 const corsOptions = {
